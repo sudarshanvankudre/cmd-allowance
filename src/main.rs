bdc1d0747f8c07e727c687a9c9a548eff933b002
Sudarshan <sudarshansvankudre@gmail.com>
2021-10-17 00:47:10 -0700

    Working on new transaction impl

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,34 @@ fn main() {
     }
 }
 
-fn read_input() -> io::Result<()> {
+fn read_input() -> io::Result<Action> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
-    println!("You typed: {}", input.trim());
-    Ok(())
+    println!("You typed: {:?}", input.trim().split(" "));
+    let mut input_list = input.trim().split(" ");
+    let cmd = input_list.next().unwrap();
+    let vals = input_list.map(|x| x.to_string()).collect();
+    let action = Action {
+        command: cmd.to_string(),
+        values: vals
+    };
+    Ok(action)
+}
+
+fn run_action(action: &Action) {
+    match action.command {
+        "new" => create_new_transaction(&action.values),
+        _ => ()
+
+    }
+}
+
+fn create_new_transaction(values: &Vec<String>) {
+    let subject = values.get(0);
+    
+}
+
+struct Action {
+    command: String,
+    values: Vec<String>
 }
